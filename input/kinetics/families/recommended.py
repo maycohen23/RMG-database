db161765e8ce316233a5a75a11444597980d3079
Alon Grinberg Dana <alon@technion.ac.il>
2022-01-10 09:04:43 +0200

    Modified recommended families

--- input/kinetics/families/recommended.py
+++ input/kinetics/families/recommended.py
@@ -17,10 +17,7 @@ default = {
     '1,3_Insertion_RSR',
     '1,4_Cyclic_birad_scission',
     '1,4_Linear_birad_scission',
-    '2+2_cycloaddition_CCO',
-    '2+2_cycloaddition_CO',
-    '2+2_cycloaddition_CS',
-    '2+2_cycloaddition_Cd',
+    '2+2_cycloaddition',
     'Birad_recombination',
     'CO_Disproportionation',
     'Birad_R_Recombination',
@@ -76,30 +73,7 @@ api = {
     '1,3_NH3_elimination',  # only forward
     'API_1,4_Cyclic_birad_scission',  # only forward
     'API_1,4_Linear_birad_scission',  # only forward
-    'API_2+2_cycloaddition_CCO',  # only reverse
-    'API_2+2_cycloaddition_CO',  # only reverse
-    'API_2+2_cycloaddition_CS',  # only reverse
-    'API_2+2_cycloaddition_Cd',  # only reverse
-    '6_membered_central_C-C_shift',
-    'API_Birad_R_Recombination',  # only forward
-    'API_Birad_recombination',  # only forward
-    'API/API_1+2_Cycloaddition',  # exclude aromatic rings
-    '1,2-Birad_to_alkene',  # only forward by default
-    '1,2_Insertion_CO',
-    '1,2_Insertion_carbene',
-    'API_1,2_NH3_elimination',
-    '1,2_shiftC',
-    '1,2_shiftS',
-    '1,3_Insertion_CO2',
-    'API_1,3_Insertion_ROR',  # exclude aromatic rings
-    'API_1,3_Insertion_RSR',  # exclude aromatic rings
-    '1,3_NH3_elimination',  # only forward
-    'API_1,4_Cyclic_birad_scission',  # only forward
-    'API_1,4_Linear_birad_scission',  # only forward
-    'API_2+2_cycloaddition_CCO',  # only reverse
-    'API_2+2_cycloaddition_CO',  # only reverse
-    'API_2+2_cycloaddition_CS',  # only reverse
-    'API_2+2_cycloaddition_Cd',  # only reverse
+    'API_2+2_cycloaddition',  # only reverse
     '6_membered_central_C-C_shift',
     'API_Birad_R_Recombination',  # only forward
     'API_Birad_recombination',  # only forward
@@ -112,21 +86,6 @@ api = {
     'API_HO2_Elimination_from_PeroxyRadical',
     'API_H_Abstraction',  # exclude aromatic rings, don't abstract from a radical site
     'API_Intra_2+2_cycloaddition_Cd',  # only reverse
-    'Intra_5_membered_conjugated_C=C_C=C_addition',
-    'Intra_Diels_alder_monocyclic',
-    'Intra_Retro_Diels_alder_bicyclic',
-    'API_Intra_Disproportionation',  # only forward
-    # 'API_Intra_RH_Add_Endocyclic', not recommended for APIs
-    # 'API_Intra_RH_Add_Exocyclic', not recommended for APIs
-    'API_Intra_R_Add_Endocyclic',  # exclude aromatic rings
-    'API_Intra_R_Add_Exocyclic',  # exclude aromatic rings
-    'Intra_R_Add_Exo_scission',
-    'Intra_ene_reaction',
-    'API_intra_H_migration',
-    'API/API_Disproportionation',  # only forward
-    'API/API_HO2_Elimination_from_PeroxyRadical',
-    'API/API_H_Abstraction',  # exclude aromatic rings, don't abstract from a radical site
-    'API/API_Intra_2+2_cycloaddition_Cd',  # only reverse
     'Intra_5_membered_conjugated_C=C_C=C_addition',
     'Intra_Diels_alder_monocyclic',
     'Intra_Retro_Diels_alder_bicyclic',
